597dfa21c1dc51ca74aea0e33a02e3eb23f61d99
Aimar M <aimarmontagut@gmail.com>
2025-12-28 20:04:15 +0800

    include main-color and change it in everything

--- assets/js/main.js
+++ assets/js/main.js
@@ -316,7 +316,7 @@ function initActiveNav() {
         navLinks.forEach((link) => {
           link.style.color =
             link.getAttribute("href") === `#${id}`
-              ? "var(--color-accent)" // Highlighted color
+              ? "var(--main-color)" // Highlighted color
               : ""; // Reset to default (inherits from CSS)
         });
       }
